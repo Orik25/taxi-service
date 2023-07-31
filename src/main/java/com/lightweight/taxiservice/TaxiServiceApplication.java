c4406f69b746110d7e67c9404baf8e7bf5af0951
Orik25 <orestpilavka@gmail.com>
2023-07-31 21:24:00 +0300

    init commit

---
+++ src/main/java/com/lightweight/taxiservice/TaxiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lightweight.taxiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lightweight/taxiservice/TaxiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightweight.taxiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
