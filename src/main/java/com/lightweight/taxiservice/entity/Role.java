78d47770abb8912773a67c558a9e2b9a82bdf294
Oleksii Siianchuk <jleha25@ukr.net>
2023-08-02 21:37:53 +0300

    - created two entities: User and Role

---
+++ src/main/java/com/lightweight/taxiservice/entity/Role.java
@@ -0,0 +1,53 @@
+package com.lightweight.taxiservice.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
+
+    public Role() {
+    }
+
+    public Role(String name, List<User> users) {
+        this.name = name;
+        this.users = users;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
+
