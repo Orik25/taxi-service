ef1dbbc336959f1d62804323b239853f9693a338
Oleksii Siianchuk <jleha25@ukr.net>
2023-08-05 21:19:02 +0300

    test files

---
+++ src/main/java/com/lightweight/taxiservice/controller/Hello.java
@@ -0,0 +1,19 @@
+package com.lightweight.taxiservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class Hello {
+
+    @GetMapping("/hello")
+    public String helloPage(){
+        return "hello";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(){
+        return "login";
+    }
+}
