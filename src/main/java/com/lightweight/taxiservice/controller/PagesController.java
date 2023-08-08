a4e77e1285f438affbe449720e62453c9fe0a265
Orik25 <orestpilavka@gmail.com>
2023-08-08 21:03:33 +0300

    set correct login direction on pages

--- src/main/java/com/lightweight/taxiservice/controller/PagesController.java
+++ src/main/java/com/lightweight/taxiservice/controller/PagesController.java
@@ -2,10 +2,9 @@ package com.lightweight.taxiservice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class Hello {
+public class PagesController {
 
     @GetMapping("/")
     public String helloPage(){
