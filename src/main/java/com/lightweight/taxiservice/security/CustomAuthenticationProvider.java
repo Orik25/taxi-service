008587bc83ac357b1cb9e390be8a4992647e52b3
Orik25 <orestpilavka@gmail.com>
2023-08-23 17:46:19 +0300

    add validation for login fix bug with access for random password

--- src/main/java/com/lightweight/taxiservice/security/CustomAuthenticationProvider.java
+++ src/main/java/com/lightweight/taxiservice/security/CustomAuthenticationProvider.java
@@ -1,31 +1,47 @@
 package com.lightweight.taxiservice.security;
 
+import com.lightweight.taxiservice.exception.CustomAuthenticationException;
+import com.lightweight.taxiservice.exception.NoUserFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     private CustomUserDetailsService userDetailsService;
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public CustomAuthenticationProvider(CustomUserDetailsService userDetailsService) {
+    public CustomAuthenticationProvider(CustomUserDetailsService userDetailsService,BCryptPasswordEncoder passwordEncoder) {
         this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    public Authentication authenticate(Authentication authentication) throws CustomAuthenticationException {
         String email = authentication.getName();
         String password = authentication.getCredentials().toString();
 
-        UserDetails  userDetails = userDetailsService.loadUserByUsername(email);
 
-        return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+        try {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+            checkPassword(password,userDetails);
+            return new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+        } catch (NoUserFoundException e) {
+            throw new CustomAuthenticationException("Authentication failed: " + e.getMessage());
+        }
+    }
+    private void checkPassword(String password,UserDetails userDetails){
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new CustomAuthenticationException("Authentication failed: Invalid password");
+        }
     }
 
     @Override
